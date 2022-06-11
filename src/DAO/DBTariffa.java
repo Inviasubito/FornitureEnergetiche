d31ed73ad4107110f5217412e59b112f22008f24
TizioBj <Inviasubito@users.noreply.github.com>
2022-06-11 15:24:50 +0200

    Update DBTariffa.java

--- src/DAO/DBTariffa.java
+++ src/DAO/DBTariffa.java
@@ -1,5 +1,9 @@
-package Entity;
+package DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Package_ausiliario.EccezioneTariffa;
 import Package_ausiliario.TipoFornitura;
 
 public class DBTariffa {
@@ -13,6 +17,35 @@ public class DBTariffa {
 		this.citta = citta;
 		this.tipoFornitura = tipoFornitura;
 		this.tariffa = tariffa;
+	}
+	
+	public DBTariffa(String citta, TipoFornitura tipoFornitura) throws EccezioneTariffa {
+		this.citta = citta;
+		this.tipoFornitura = tipoFornitura;
+		caricaDaDBwithCittaAndTipoFornitura();
+		
+	}
+	
+	public void caricaDaDBwithCittaAndTipoFornitura() throws EccezioneTariffa {
+		String query = "SELECT * FROM Tariffe WHERE citta='"+this.citta+"' AND tipoFornitura=" + this.tipoFornitura.ordinal() +";";
+
+
+			try {
+				ResultSet rs = DBConnectionManager.selectQuery(query);
+				
+				if(rs.next()) { //se ho un risultato
+					
+					//mi vado a prendere i dati, accedendo tramite il nome dell'attributo-colonna
+					this.tariffa = rs.getDouble("tariffa");
+				}else {
+					throw new EccezioneTariffa("Tariffa not found");
+				}
+			
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 	}
 
 	public String getCitta() {
@@ -37,5 +70,19 @@ public class DBTariffa {
 
 	public void setTariffa(double tariffa) {
 		this.tariffa = tariffa;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		DBTariffa tariffa = null;
+	try {
+		tariffa = new DBTariffa("NAPOLI" , TipoFornitura.ENERGIA_ELETTRICA);
+		System.out.println(tariffa.getTariffa());
+	} catch (EccezioneTariffa e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 }
